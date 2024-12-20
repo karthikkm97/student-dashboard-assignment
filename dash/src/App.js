9f0b61809ab6cddeffd8d55488f1b5dbe5432956
karthikkm97 <66115826+karthikkm97@users.noreply.github.com>
2024-12-20 21:27:51 +0530

    Update App.js

--- dash/src/App.js
+++ dash/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import StudentsTable from './components/StudentsTable'; // Fix typo here
+import StudentsTable from './components/StudentsTable';
 
 const App = () => {
   return (
